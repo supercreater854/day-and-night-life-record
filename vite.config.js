29e8add9f52379b4432d6645dedd44109211c333
supercreater854 <1904578478@qq.com>
2026-09-07 12:23:27 +0800

    Improve Android browser CSS compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({ plugins: [react()], base: './' });
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: { cssTarget: 'chrome61' },
+});
